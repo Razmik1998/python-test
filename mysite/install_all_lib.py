564f0ba0a42506e802eac71dd7f0326cf79a0b3e
razmik1998 <nubaryan.razmik89@gmail.com>
2021-07-28 18:08:07 +0400

    add requarments.txt

---
+++ mysite/install_all_lib.py
@@ -0,0 +1,34 @@
+import os
+import time
+import sys
+import fnmatch
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+from multiprocessing.dummy import Pool as ThreadPool
+print('ok')
+"""1 pip install pipreqs
+   2 pipreqs .
+   3 pip install -r requirements.txt
+   python3 manage.py runserver 192.168.0.109:8081
+   """
+# channels==3.0.3
+# requests==2.22.0
+# keyring==18.0.1
+# PyMySQL==1.0.2
+# validate_email==1.3
+# Django==2.2.12
+# pytz==2019.3
+# asgiref==3.3.4
+# backports.statistics==0.1.0
+# beautifulsoup4==4.9.3
+# Cython==0.29.23
+# djangorestframework==3.12.4
+# ipaddr==2.2.0
+# mypy_extensions==0.4.3
+# numpy==1.20.2
+# ordereddict==1.1
+# PyJWT==2.1.0
+# python_dateutil==2.8.1
+# secrets==1.0.2
+# wincertstore==0.2
